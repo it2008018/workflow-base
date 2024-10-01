5adc5d8b39adcfa2bcc2acbccfae65e840511c46
it2008018 <51204924+it2008018@users.noreply.github.com>
2024-10-01 14:15:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete",
+    ]
+  }
+}
+
+variable "tenant_id" {}
+variable "object_id" {}
 
